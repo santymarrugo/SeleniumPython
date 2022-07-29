0af65f1ef228c6536aad667ceaa702eae5f84224
Santys1983## <santymarrugo@gmail.com>
2022-07-29 15:46:55 -0500

    se agrega archivo settings

--- PagesConty/PageAsientos.py
+++ PagesConty/PageAsientos.py
@@ -14,20 +14,22 @@ class PageAsientos:
 
         self.btnMenuPrincipal = (By.XPATH, "//span[contains(@id,'iconMenu_topToggle')]")
         self.opCarpetas = (By.XPATH, "//a[contains(@id,'v-pills-carpetas-tab')]")
-        self.opAsiento = (By.XPATH, "(//a[@class='nav-link menuTopToggleSubOption menuTopToggleText'][contains(.,'Asientos')])[3]")
+        self.opAsiento = (
+        By.XPATH, "(//a[@class='nav-link menuTopToggleSubOption menuTopToggleText'][contains(.,'Asientos')])[3]")
         self.labelAsientoModal = (By.XPATH, "//LABEL[@id='exampleModalLongTitleAsientos']")
         self.rbCrearMes = (By.XPATH, "//input[contains(@id,'inputAsiento1')]")
         self.inputMesAnio = (By.XPATH, "//input[contains(@id,'FechaNuevoAsiento')]")
-        self.btnAceptarAsientos = (By.XPATH, "//button[contains(@class,'btn btn-danger popupBotonAceptar btnCargarAsientos')]")
+        self.btnAceptarAsientos = (
+        By.XPATH, "//button[contains(@class,'btn btn-danger popupBotonAceptar btnCargarAsientos')]")
         self.rbEditarMesAsiento = (By.XPATH, "//input[contains(@id,'inputAsiento2')]")
         self.rbPrimerMes = (By.XPATH, "//input[contains(@value,'1/2022')]")
         self.lblVerificarMesAsiento = (By.XPATH, "//LABEL[@id='inicio']")
         self.btnNuevaLinea = (By.XPATH, "//i[@class='material-icons'][contains(.,'note_add')]")
 
-
     '''
     Metodo para acceder a la opcion Asientos y que se muestre el PopUp para escoger la fecha del asiento
     '''
+
     def accederAsientos(self):
         self.driver.find_element(*self.btnMenuPrincipal).click()
         WebDriverWait(self.driver, 5).until(expected_conditions.element_to_be_clickable(self.opCarpetas))
@@ -38,6 +40,7 @@ class PageAsientos:
     Metodo para verificar que texto al momento de hacer click en la opcion asientos, y asegurar que se haya ingresado
     exitosamente a esa opcion.
     '''
+
     # Metodo para verificar que se esté mostrando el modal para elegir el mes del asiento
     def verificarTituloVentanaModalAsiento(self, txtmensajeModal):
         tcAssert = unittest.TestCase("__init__")
@@ -56,7 +59,8 @@ class PageAsientos:
 
     # Metodo para verificar el texto de la parte superior izquierda y que nos indica en que parte de la plataforma estamos
     def verificarTituloVentana(self, mensaje):
-        WebDriverWait(self.driver, 5).until(expected_conditions.text_to_be_present_in_element(self.lblVerificarMesAsiento, mensaje))
+        WebDriverWait(self.driver, 5).until(
+            expected_conditions.text_to_be_present_in_element(self.lblVerificarMesAsiento, mensaje))
         tcAssert = unittest.TestCase("__init__")
         lblMesAsiento = self.driver.find_element(*self.lblVerificarMesAsiento).text
         tcAssert.assertEqual(lblMesAsiento, mensaje)
@@ -69,24 +73,23 @@ class PageAsientos:
     def escribirLineaAsiento(self):
         self.driver.execute_script('hot.selectCell(0,5)')
         self.driver.execute_script('hot.setDataAtCell(0,5,"12")')
-        
-        # self.driver.execute_script('hot.setDataAtCell(0,6,"11111")')
-        # self.driver.execute_script('hot.setDataAtCell(0,7,"41113")')
-        # self.driver.execute_script('hot.setDataAtCell(0,9,"1")')
-        # self.driver.execute_script('hot.setDataAtCell(0,10,"Prueba Selenium con Python")')
-        # self.driver.execute_script('hot.setDataAtCell(0,11,"1")')
-        # self.driver.execute_script('hot.setDataAtCell(0,12,"12500")')
-        # self.driver.execute_script('hot.setDataAtCell(0,19,"I")')
-        # PageAsientos(self.driver).clickAgregarLineaAsiento()
-        # self.driver.execute_script('hot.selectCell(1,5)')
-        # self.driver.execute_script('hot.setDataAtCell(1,5,"12")')
-        # self.driver.execute_script('hot.setDataAtCell(1,6,"11111")')
-        # self.driver.execute_script('hot.setDataAtCell(1,7,"41113")')
-        # self.driver.execute_script('hot.setDataAtCell(1,9,"1")')
-        # self.driver.execute_script('hot.setDataAtCell(1,10,"Prueba Selenium con Python_linea2")')
-        # self.driver.execute_script('hot.setDataAtCell(1,11,"1")')
-        # self.driver.execute_script('hot.setDataAtCell(1,12,"50000")')
-        # self.driver.execute_script('hot.setDataAtCell(1,19,"I")')
+        self.driver.execute_script('hot.setDataAtCell(0,6,"11111")')
+        self.driver.execute_script('hot.setDataAtCell(0,7,"41113")')
+        self.driver.execute_script('hot.setDataAtCell(0,9,"1")')
+        self.driver.execute_script('hot.setDataAtCell(0,10,"Prueba Selenium con Python")')
+        self.driver.execute_script('hot.setDataAtCell(0,11,"1")')
+        self.driver.execute_script('hot.setDataAtCell(0,12,"12500")')
+        self.driver.execute_script('hot.setDataAtCell(0,19,"I")')
+        PageAsientos(self.driver).clickAgregarLineaAsiento()
+        self.driver.execute_script('hot.selectCell(1,5)')
+        self.driver.execute_script('hot.setDataAtCell(1,5,"12")')
+        self.driver.execute_script('hot.setDataAtCell(1,6,"11111")')
+        self.driver.execute_script('hot.setDataAtCell(1,7,"41113")')
+        self.driver.execute_script('hot.setDataAtCell(1,9,"1")')
+        self.driver.execute_script('hot.setDataAtCell(1,10,"Prueba Selenium con Python_linea2")')
+        self.driver.execute_script('hot.setDataAtCell(1,11,"1")')
+        self.driver.execute_script('hot.setDataAtCell(1,12,"50000")')
+        self.driver.execute_script('hot.setDataAtCell(1,19,"I")')
 
     # Metodo para hacer click en el radiobutton para crear un mes nuevo de asientos
     def clickCrearMes(self):
@@ -99,7 +102,3 @@ class PageAsientos:
         self.driver.find_element(*self.inputMesAnio).send_keys(Keys.CONTROL + "a")
         self.driver.find_element(*self.inputMesAnio).send_keys(Keys.BACKSPACE)
         self.driver.find_element(*self.inputMesAnio).send_keys(fecha)
-
-
-
-
